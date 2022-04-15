8943d4320ebf7a44a9ae4532bf65aa23ac79e5f4
Roberto García <rgarmor@gmail.com>
2022-04-15 10:58:38 -0500

    Edit commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Article = require('./models/article');
+const articleRouter = require('./routes/articles');
+const app = express();
+
+mongoose.connect('mongodb://localhost/blog',  err => {
+    if(err) throw err;
+    console.log('connected to MongoDB')
+});
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended : false }));
+
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000);
